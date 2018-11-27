987a271424b406681559a8db4b4309ea76852e68
EDUARDO MENDOZA <rl238513@rlas-macair-36.rlas-116.org>
2018-11-27 10:25:40 -0600

    Initial Commit

---
+++ TipCalculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TipCalculator
+//
+//  Created by EDUARDO MENDOZA on 11/27/18.
+//  Copyright © 2018 EDUARDO MENDOZA. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
